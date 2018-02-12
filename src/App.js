a0848114c4a737c32af9fee3bbbde035d4e29a2e
Dania <thatdania@Danias-MacBook-Pro.local>
2018-02-12 16:37:52 +0000

    Add a feature which shows the letter in a div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Validation from './Validation/Validation'
+import Char from './Char/Char'
 import './App.css';
 
 class App extends Component {
@@ -14,11 +15,17 @@ class App extends Component {
 
 
   render() {
+
+    const charList = this.state.input.split('').map((ch,index)=> {
+      return <Char character={ch} key={index}/>;
+    });
+
     return (
       <div className="App">
         <input className="Lengthfield" onChange={this.stringLengthHandler} type="text" />
         <p> {this.state.input} </p>
         <Validation inputLength={this.state.input.length}/>
+        {charList}
       </div>
     );
   }
