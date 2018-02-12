6a050fa5914d8f1dca62a989e340ff68e407e59f
Dania <thatdania@Danias-MacBook-Pro.local>
2018-02-12 17:04:36 +0000

    made a color randomiser for the font. crazy stuff

--- src/Char/Char.js
+++ src/Char/Char.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Char.css'
 
 const char = (props) => {
+
+    const fontColor = '#' + Math.floor(Math.random()*6777260).toString(16);
 
     const style = {
         display: 'inline-block',
         padding: '16px',
         margin: '16px',
         border: '1px solid black',
-        textAlign: 'center'
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: '16px',
+        color: fontColor
+
     };
 
+
     return (
-        <div style={style} onClick={props.click}> {props.character} </div>
+        <div className="scramble" style={style} onClick={props.click}> {props.character} </div>
     );
 }
 
